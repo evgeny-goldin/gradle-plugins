0e009b1a2c23643d94671ec1079903a93496701d
Evgeny Goldin <evgenyg@gmail.com>
2011-05-17 18:50:23 +0200

    About plugin is packed

--- src/main/groovy/com/goldin/gradle/about/AboutPlugin.groovy
+++ src/main/groovy/com/goldin/gradle/about/AboutPlugin.groovy
@@ -1,8 +1,19 @@
 package com.goldin.gradle.about
 
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+
 /**
  * Plugin that adds "about" build metadata to build artifacts.
  */
-class AboutPlugin
+class AboutPlugin implements Plugin<Project>
 {
+    @Override
+    void apply ( Project p )
+    {
+        p.task( 'about' ) << {
+            println "Generating about"
+        }
+    }
 }
