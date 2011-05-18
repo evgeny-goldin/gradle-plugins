1d4d0d0de36d226bc2eb3636a9338417d28c89f0
Evgeny Goldin <evgenyg@gmail.com>
2011-05-18 03:22:31 +0200

    "About" data is created

--- src/main/groovy/com/goldin/gradle/about/AboutTask.groovy
+++ src/main/groovy/com/goldin/gradle/about/AboutTask.groovy
@@ -2,10 +2,9 @@ package com.goldin.gradle.about
 
 import com.goldin.gcommons.GCommons
 import com.goldin.gradle.util.BaseTask
+import java.text.SimpleDateFormat
 import org.gradle.api.logging.Logger
-import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.bundling.Zip
-
 
 /**
  * {@link AboutPlugin} task
@@ -29,12 +28,40 @@ class AboutTask extends BaseTask
     String  exclude
 
 
-    @TaskAction
-    def createAbout()
+    private String padLines ( String s )
+    {
+        def padWidth = ' Status        : ['.size()
+        def lines    = s.readLines()
+
+        ( lines ? ( lines[ 0 ] + (( lines.size() > 1 ) ? '\n' + lines[ 1 .. -1 ].collect { '|' + ( ' ' * padWidth ) + it }.join( '\n' ) :
+                                                         '' )) :
+                  '' )
+    }
+
+
+    private String exec ( String command, File directory = null )
+    {
+        def process = command.execute(( List ) null, directory )
+        ( process.text + process.err.text ).trim()
+    }
+
+
+    private String find ( String prefix, String command ) { find( prefix, exec( command ).readLines()) }
+    private String find ( String prefix, List<String> l ) { l.find{ it.startsWith( prefix ) }?.replace( prefix, '' )?.trim() ?: '' }
+    private String sort ( Map<String,String> map )
+    {
+        def maxKey = map.keySet()*.size().max() + 3
+        map.sort().collect { String key, String value ->
+                             "[$key]".padRight( maxKey ) + ":[$value]" }.
+                   join( '\n' )
+    }
+
+
+    void taskAction()
     {
         Zip zipTask   = task( 'jar', Zip )
         directory     = directory ?: zipTask.destinationDir
-        fileName      = fileName  ?: "about-${project.group}-${project.name}-${project.version}.txt"
+        fileName      = fileName  ?: "about-${group}-${name}-${version}.txt"
         def split     = { String s -> ( List<String> )( s ? s.split( /,/ ).toList()*.trim().findAll{ it } : null ) }
         def files     = GCommons.file().files( directory, split( include ), split( exclude ))
         def tempFile  = new File( zipTask.temporaryDir, fileName )
@@ -67,18 +94,197 @@ class AboutTask extends BaseTask
     }
 
 
-    String serverContent ()
+    String jenkinsContent()
     {
-        'sss'
+        // https://wiki.jenkins-ci.org/display/JENKINS/Building+a+software+project
+
+        """
+        |===============================================================================
+        | Jenkins Info
+        |===============================================================================
+        | Server        : [${ env[ 'JENKINS_URL' ] }]
+        | Job           : [${ env[ 'JENKINS_URL' ] }job/${ env[ 'JOB_NAME' ] }/${ env[ 'BUILD_NUMBER' ]}/]
+        | Log           : [${ env[ 'JENKINS_URL' ] }job/${ env[ 'JOB_NAME' ] }/${ env[ 'BUILD_NUMBER' ]}/console]"""
+    }
+
+
+    String hudsonContent()
+    {
+        // http://weblogs.java.net/blog/johnsmart/archive/2008/03/using_hudson_en.html
+
+        """
+        |===============================================================================
+        | Hudson Info
+        |===============================================================================
+        | Server        : [${ env[ 'HUDSON_URL' ] }]
+        | Job           : [${ env[ 'HUDSON_URL' ] }job/${ env[ 'JOB_NAME' ] }/${ env[ 'BUILD_NUMBER' ]}/]
+        | Log           : [${ env[ 'HUDSON_URL' ] }job/${ env[ 'JOB_NAME' ] }/${ env[ 'BUILD_NUMBER' ]}/console]"""
+    }
+
+
+    String teamcityContent()
+    {
+        // http://confluence.jetbrains.net/display/TCD65/Predefined+Build+Parameters
+
+        """
+        |===============================================================================
+        | TeamCity Info
+        |===============================================================================
+        | Project Name  : [${ env[ 'TEAMCITY_PROJECT_NAME' ] }]
+        | Build Config  : [${ env[ 'TEAMCITY_BUILDCONF_NAME' ] }]
+        | Build Number  : [${ env[ 'BUILD_NUMBER' ] }]"""
+    }
+
+
+    String serverContent()
+    {
+        ( env[ 'JENKINS_URL'      ] ? jenkinsContent()  :
+          env[ 'HUDSON_URL'       ] ? hudsonContent()   :
+          env[ 'TEAMCITY_VERSION' ] ? teamcityContent() :
+                                      '' )  +
+        """
+        |==============================================================================="""
     }
 
     String buildContent ()
     {
-        'bbb'
+        def props  = System.properties
+        def format = new SimpleDateFormat( "dd MMM, EEEE, yyyy, HH:mm:ss (zzzzzz:'GMT'ZZZZZZ)", Locale.ENGLISH )
+
+        """
+        |===============================================================================
+        | Build Info
+        |===============================================================================
+        | Host          : [${ env[ 'COMPUTERNAME' ] ?: env[ 'HOSTNAME' ] ?: exec( 'hostname' ) ?: '' }]
+        | Build Time    : [${ format.format( new Date()) }]
+        | User          : [${ props[ 'user.name' ] }]
+        | ${ dumpPaths ? 'Directory     : [' + props[ 'user.dir' ] + ']': '' }
+        | Java          : [${ props[ 'java.version' ] }][${ props[ 'java.vm.vendor' ] }]${ dumpPaths ? '[' + props[ 'java.home' ] + ']' : '' }[${ props[ 'java.vm.name' ] }]
+        | OS            : [${ props[ 'os.name' ] }][${ props[ 'os.arch' ] }][${ props[ 'os.version' ] }]
+        |===============================================================================
+        | Gradle Info
+        |===============================================================================
+        | ${ dumpPaths ? 'Home          : [' + project.gradle.gradleHomeDir.canonicalPath + ']' : '' }
+        | GRADLE_OPTS   : [${ env[ 'GRADLE_OPTS' ] ?: '' }]
+        | Version       : [${ project.gradle.gradleVersion }]
+        | Project       : [${ dumpPaths ? project.toString() : project.toString().replaceAll( /\s+@.+/, '' )}]
+        | Tasks         : ${ project.gradle.startParameter.taskNames }
+        | ${ dumpPaths ? 'Basedir       : [' + rootDir.canonicalPath + ']': '' }
+        | Coordinates   : [$group:$name:$version]
+        | ${ dumpDependencies ? 'Dependencies  : [Not Yet]' : '' }""" +
+
+        ( dumpSystem ?
+
+        """
+        |===============================================================================
+        | System Properties
+        |===============================================================================
+        |${ sort( props ) }""" : '' ) +
+
+        ( dumpEnv ?
+
+        """
+        |===============================================================================
+        | Environment Variables
+        |===============================================================================
+        |${ sort( env ) }""" : '' )
     }
 
-    String scmContent ()
+
+    String scmContent()
     {
-        'scm'
+        if ( ! dumpSCM ) { return '' }
+
+        File   svnDir           = new File( project.rootDir, '.svn' )
+        String svnVersion       = null
+        String svnStatus        = null
+        String gitVersion       = null
+        String gitStatusCommand = null
+        String gitStatus        = null
+
+        /**
+         * Trying SVN
+         */
+
+        if ( svnDir.isDirectory())
+        {
+            svnVersion = exec( 'svn --version' )
+            if ( svnVersion.contains( 'svn, version' ))
+            {
+                svnStatus = exec( "svn status ${}.canonicalPath" )
+                if (( ! svnStatus.contains( 'is not a working copy' )) &&
+                    ( ! svnStatus.contains( 'containing working copy admin area is missing' )))
+                {
+                    return svnContent( svnStatus )
+                }
+            }
+        }
+
+        /**
+         * Trying Git
+         */
+
+        gitVersion = exec( 'git --version' )
+
+        if ( gitVersion.contains( 'git version' ))
+        {
+            gitStatusCommand = "git status" + ( gitStatusProject ? '' : ' ' + rootDir.canonicalPath )
+            gitStatus        = exec( gitStatusCommand )
+
+            if ( ! gitStatus.contains( 'fatal: Not a git repository' ))
+            {
+                return gitContent( gitStatus )
+            }
+        }
+
+        """
+        |===============================================================================
+        | SCM Info
+        |===============================================================================
+        | Unsupported SCM system: either project is not managed by SVN/Git or corresponding command-line clients are not available.
+        | Tried SVN:
+        | ~~~~~~~~~~
+        | [$svnDir.canonicalPath] - ${ svnDir.isDirectory() ? 'found' : 'not found' }
+        | ${ svnVersion ? '"svn --version" returned [' + svnVersion + ']'                           : '' }
+        | ${ svnStatus  ? '"svn status ' + rootDir.canonicalPath + '" returned [' + svnStatus + ']' : '' }
+        | Tried Git:
+        | ~~~~~~~~~~
+        | ${ gitVersion ? '"git --version" returned [' + gitVersion + ']'                            : '' }
+        | ${ gitStatus  ? '"' + gitStatusCommand + '" returned [' + gitStatus + ']'                  : '' }"""
+    }
+
+
+    String svnContent( String svnStatus )
+    {
+        def svnInfo = exec( "svn info ${rootDir.canonicalPath}" ).readLines()
+        def commit  = exec( 'svn log -l 1' ).readLines()[ 1 ]
+
+        """
+        |===============================================================================
+        | SVN Info
+        |===============================================================================
+        | Repository    : [${ find( 'URL:',      svnInfo )}]
+        | Revision      : [${ find( 'Revision:', svnInfo )}]
+        | Status        : [${ padLines( svnStatus ) }]
+        | Last Commit   : [$commit]
+        | Commit Date   : [${ commit.split( '\\|' )[ 2 ].trim() }]
+        | Commit Author : [${ commit.split( '\\|' )[ 1 ].trim() }]"""
+    }
+
+
+    String gitContent( String gitStatus )
+    {
+        def gitLog = exec( 'git log -1' ).readLines()
+
+        """
+        |===============================================================================
+        | Git Info
+        |===============================================================================
+        | Repositories  : [${ padLines( exec( 'git remote -v' )) }]
+        | Branch        : [${ find( '# On branch', 'git status' ) }]
+        | ${ gitStatusProject ? 'Project' : 'Basedir' } Status: [${ padLines( gitStatus ) }]
+        | Last Commit   : [${ find( 'commit',      gitLog )}]
+        | Commit Date   : [${ find( 'Date:',       gitLog )}]
+        | Commit Author : [${ find( 'Author:',     gitLog )}]"""
     }
 }
--- src/main/groovy/com/goldin/gradle/util/BaseTask.groovy
+++ src/main/groovy/com/goldin/gradle/util/BaseTask.groovy
@@ -1,16 +1,39 @@
 package com.goldin.gradle.util
 
 import com.goldin.gcommons.GCommons
+import com.goldin.gcommons.beans.VerifyBean
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
 
 /**
  * Base helper task class to be extended by other tasks
  */
-class BaseTask extends DefaultTask
+abstract class BaseTask extends DefaultTask
 {
+    File       rootDir
+    String     version
+    VerifyBean verify = GCommons.verify()
 
     public <T> T task ( String name, Class<T> type )
     {
-        GCommons.verify().isInstance(( project.getTasksByName( name, true ) as List ).first(), type )
+        verify.isInstance(( project.getTasksByName( name, true ) as List ).first(), type )
     }
+
+
+    @TaskAction
+    def doTask()
+    {
+        this.rootDir = verify.notNull( project.rootDir )
+        this.group   = verify.notNull( project.group   )
+        this.name    = verify.notNull( project.name    )
+        this.version = verify.notNull( project.version )
+        taskAction()
+    }
+
+
+    /**
+     * Should be implemented by task.
+     * Called after all fields are initialized.
+     */
+    abstract void taskAction()
 }
