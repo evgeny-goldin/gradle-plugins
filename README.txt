53defaf1a81ff89e16aa80def2cfd757ac65fb19
Evgeny Goldin <evgenyg@gmail.com>
2011-06-13 01:27:16 +0300

    "gradle-about-plugin:0.1-RC"

--- README.txt
+++ README.txt
@@ -1,3 +1,23 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Gradle plugins
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ -=-= "gradle-about-plugin"=-=-
+---------------------------------
+apply plugin: 'about'
+
+..
+
+buildscript {
+    repositories { mavenRepo urls: 'http://evgeny-goldin.org/artifactory/repo/' }
+    dependencies { classpath       'com.goldin.plugins:gradle:0.1-RC' }
+}
+
+..
+
+about  { dumpDependencies = true }
+jar << { about.execute() }
+---------------------------------
+Or run
+
+> gradle clean build about
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
--- src/main/groovy/com/goldin/plugins/gradle/util/BaseTask.groovy
+++ src/main/groovy/com/goldin/plugins/gradle/util/BaseTask.groovy
@@ -1,37 +1,79 @@
 package com.goldin.plugins.gradle.util
 
-import com.goldin.gcommons.GCommons
-import com.goldin.gcommons.beans.VerifyBean
+import org.apache.tools.ant.DirectoryScanner
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
-import org.gradle.api.tasks.bundling.Zip
+import org.gradle.api.tasks.bundling.Jar
+
 
 /**
  * Base helper task class to be extended by other tasks
  */
 abstract class BaseTask extends DefaultTask
 {
-    File       rootDir
-    String     version
-    Zip        zipTask
-    VerifyBean verify = GCommons.verify()
-
-
-    public <T> T task ( String name, Class<T> type )
-    {
-        verify.isInstance(( project.getTasksByName( name, true ) as List ).first(), type )
-    }
+    File   rootDir
+    String version
+    Jar    jarTask
 
 
     @TaskAction
+//    @Requires({ project.rootDir && project.group && project.name && project.version })
     def doTask()
     {
-        this.rootDir = verify.notNull( project.rootDir )
-        this.group   = verify.notNull( project.group   )
-        this.name    = verify.notNull( project.name    )
-        this.version = verify.notNull( project.version )
-        this.zipTask = task( 'jar', Zip )
+        this.rootDir = project.rootDir
+        this.group   = project.group
+        this.name    = project.name
+        this.version = project.version
+        this.jarTask = ( Jar ) project.tasks[ 'jar' ]
         taskAction()
+    }
+
+    
+    /**
+     * Retrieves files (and directories, if required) given base directory and inclusion/exclusion patterns.
+     * Symbolic links are not followed.
+     *
+     * @param baseDirectory      files base directory
+     * @param includePatterns    patterns to use for including files, all files are included if null
+     * @param excludePatterns    patterns to use for excluding files, no files are excluded if null
+     * @param isCaseSensitive    whether or not include and exclude patterns are matched in a case sensitive way
+     * @param includeDirectories whether directories included should be returned as well
+     * @param failIfNotFound     whether execution should fail if no files were found
+     *
+     * @return files under base directory specified passing an inclusion/exclusion patterns
+     */
+    List<File> files ( File         baseDirectory,
+                       List<String> includePatterns    = null,
+                       List<String> excludePatterns    = null,
+                       boolean      isCaseSensitive    = true,
+                       boolean      includeDirectories = false,
+                       boolean      failIfNotFound     = true )
+    {
+        assert baseDirectory.isDirectory(), "Directory [$baseDirectory] doesn't exist"
+        def scanner = new DirectoryScanner()
+
+        scanner.with {
+            basedir           = baseDirectory
+            includes          = includePatterns as String[]
+            excludes          = excludePatterns as String[]
+            caseSensitive     = isCaseSensitive
+            errorOnMissingDir = true
+            followSymlinks    = false
+            scan()
+        }
+
+        def files = []
+        scanner.includedFiles.each { String filePath -> files << new File( baseDirectory, filePath ) }
+
+        if ( includeDirectories )
+        {
+            scanner.includedDirectories.findAll { it }.each { String dirPath -> files << new File( baseDirectory, dirPath ) }
+        }
+
+        assert ( files || ( ! failIfNotFound )), \
+               "No files are included by parent dir [$baseDirectory] and include/exclude patterns ${ includePatterns ?: [] }/${ excludePatterns ?: [] }"
+
+        files
     }
 
 
