cbb5106ff00164e4f0570d0323337395cd74d45f
Evgeny Goldin <evgenyg@gmail.com>
2012-07-15 22:58:33 +0200

    KotlinCompile updates.

--- src/main/groovy/com/github/goldin/plugins/gradle/kotlin/tasks/KotlinCompile.groovy
+++ src/main/groovy/com/github/goldin/plugins/gradle/kotlin/tasks/KotlinCompile.groovy
@@ -2,56 +2,40 @@ package com.github.goldin.plugins.gradle.kotlin.tasks
 
 import static org.jetbrains.jet.cli.common.ExitCode.*
 
-import com.google.common.io.Files
-import com.google.common.io.Resources
 import org.gradle.api.GradleException
 import org.gradle.api.tasks.compile.AbstractCompile
 import org.jetbrains.jet.cli.jvm.K2JVMCompiler
 import org.jetbrains.jet.cli.jvm.K2JVMCompilerArguments
 
 
-class KotlinCompile extends AbstractCompile {
+class KotlinCompile extends AbstractCompile
+{
 
     private final K2JVMCompiler compiler = new K2JVMCompiler()
 
 
     @Override
-    protected void compile() {
-        final args = new K2JVMCompilerArguments()
+    protected void compile()
+    {
+        final args            = new K2JVMCompilerArguments()
+        args.noStdlib         = false
+        args.noJdkAnnotations = true
+        args.classpath        = classpath.filter{ File f -> f.exists() }.asPath ?: null
+        args.sourceDirs       = (( Iterable<File> ) source )*.absolutePath
+        args.outputDir        = destinationDir.path
+        final exitCode        = compiler.exec( System.err, args )
 
-        // todo better source handling (not file-by-file)
-        final sources = source*.absolutePath
-
-        // todo what are the modes?
-        args.mode = 'stdlib'
-        final classPath = classpath.filter {it.exists()}.asPath
-        if (classPath.length() > 0) {
-            args.setClasspath(classPath)
-        }
-        args.setSourceDirs( sources )
-        args.setOutputDir( destinationDir.path )
-        args.jdkAnnotations = extractJdkHeaders()
-        final exitCode      = compiler.exec(System.err, args)
-
-        switch (exitCode) {
+        switch ( exitCode )
+        {
             case COMPILATION_ERROR:
-                throw new GradleException('Compilation error. See log for more details')
+                throw new GradleException( 'Compilation error. See log for more details' )
+
             case INTERNAL_ERROR:
-                throw new GradleException('Internal compiler error. See log for more details')
+                throw new GradleException( 'Internal compiler error. See log for more details' )
+
+            case OK:
+            default:
+                logger.info( 'Compilation successful' )
         }
-    }
-
-
-    private String extractJdkHeaders() {
-        final kotlinJdkHeaders = 'kotlin-jdk-annotations.jar'
-        final jdkHeaders       = Resources.getResource(kotlinJdkHeaders)
-
-        final File jdkHeadersTempDir = Files.createTempDir()
-        jdkHeadersTempDir.deleteOnExit()
-
-        final File jdkHeadersFile = new File(jdkHeadersTempDir, kotlinJdkHeaders)
-        Files.copy(Resources.newInputStreamSupplier(jdkHeaders), jdkHeadersFile)
-
-        jdkHeadersFile.path
     }
 }
