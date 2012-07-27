a3f0e3d720e536402fd3eb426e8731d16e84f2d7
Evgeny Goldin <evgenyg@gmail.com>
2012-07-27 18:30:35 +0200

    AssembleTeamCityPluginExtension - methods added for collecting server/agent configurations and jar tasks.

--- teamcity/src/main/groovy/com/github/goldin/plugins/gradle/teamcity/AssembleTeamCityPluginExtension.groovy
+++ teamcity/src/main/groovy/com/github/goldin/plugins/gradle/teamcity/AssembleTeamCityPluginExtension.groovy
@@ -1,15 +1,39 @@
 package com.github.goldin.plugins.gradle.teamcity
 
+import org.gradle.api.artifacts.Configuration
+import org.gradle.api.tasks.bundling.Jar
+
 
 /**
  * {@link AssembleTeamCityPluginTask} extension.
  */
+@SuppressWarnings([ 'ConfusingMethodName' ])
 class AssembleTeamCityPluginExtension
 {
-    String displayName    = ''
-    String description    = ''
-    String vendorName     = ''
-    String vendorUrl      = ''
-    File   destinationZip = null
-    String type           = 'server'
+    String displayName = ''
+    String description = ''
+    String vendorName  = ''
+    String vendorUrl   = ''
+    File   destinationZip
+
+    final List<Configuration> serverConfigurations = []
+    final List<Jar>           serverJars           = []
+    final List<Configuration> agentConfigurations  = []
+    final List<Jar>           agentJars            = []
+
+    /**
+     * Methods invoked by assembleTeamcityPluginConfig { .. } configuration closure.
+     */
+
+    void displayName    ( String displayName    ){ this.displayName    = displayName    }
+    void description    ( String description    ){ this.description    = description    }
+    void vendorName     ( String vendorName     ){ this.vendorName     = vendorName     }
+    void vendorUrl      ( String vendorUrl      ){ this.vendorUrl      = vendorUrl      }
+    void destinationZip ( File   destinationZip ){ this.destinationZip = destinationZip }
+
+    void serverConfigurations( Configuration ... configurations ){ serverConfigurations.addAll( configurations )}
+    void serverJars          ( Jar ... jars ){ serverJars.addAll( jars )}
+
+    void agentConfigurations ( Configuration ... configurations ){ agentConfigurations. addAll( configurations )}
+    void agentJars           ( Jar ... jars ){ agentJars.addAll( jars )}
 }
--- teamcity/src/main/groovy/com/github/goldin/plugins/gradle/teamcity/AssembleTeamCityPluginTask.groovy
+++ teamcity/src/main/groovy/com/github/goldin/plugins/gradle/teamcity/AssembleTeamCityPluginTask.groovy
@@ -1,8 +1,8 @@
 package com.github.goldin.plugins.gradle.teamcity
 
 import com.github.goldin.plugins.gradle.common.BaseTask
-import org.gradle.api.tasks.bundling.Jar
-
+import org.gcontracts.annotations.Ensures
+import org.gcontracts.annotations.Requires
 
 /**
  * Assembles plugin archive.
@@ -13,42 +13,78 @@ class AssembleTeamCityPluginTask extends BaseTask
     @SuppressWarnings([ 'FileCreateTempFile' ])
     void taskAction()
     {
-        final extension = ( AssembleTeamCityPluginExtension ) project[ TeamCityPlugin.ASSEMBLE_PLUGIN_EXTENSION ]
-        final pluginXml = this.class.getResourceAsStream( '/teamcity-plugin.xml' ).getText( 'UTF-8' ).
-                          replace( '@name@',         project.name ).
-                          replace( '@version@',      version.toString()).
-                          replace( '@display-name@', extension.displayName ).
-                          replace( '@description@',  extension.description ).
-                          replace( '@vendor-name@',  extension.vendorName ).
-                          replace( '@vendor-url@',   extension.vendorUrl )
+        final ext           = ( AssembleTeamCityPluginExtension ) project[ TeamCityPlugin.ASSEMBLE_PLUGIN_EXTENSION ]
+        final pluginXmlFile = createPluginXml( ext )
 
-        assert jarTask, "'jar' task is not found in project [$project]"
-
-        final pluginJars = (( Collection<Jar> )( dependsOn.findAll{ it instanceof Jar } + jarTask )).
-                           toSet().findAll { it.archivePath.file }*.archivePath
-        final tempFile   = File.createTempFile( project.name, null )
-        final zip        = extension.destinationZip ?:
-                           new File( project.buildDir, "teamcity/${ project.name }-${ version }.zip" )
-
-        tempFile.write( pluginXml )
-
-        if ( 'server' == extension.type )
+        if ( ext.serverConfigurations )
         {
-            ant.zip( destfile: zip ) {
-                zipfileset( file: tempFile, fullpath: 'teamcity-plugin.xml' )
+            final serverPlugin = archiveServerPlugin( ext, pluginXmlFile )
+            logger.info( "Server plugin created at [${ serverPlugin.canonicalPath }]" )
+        }
 
-                ( pluginJars + project.configurations.compile.files - project.configurations.teamcity.files ).each {
-                    File f ->
-                    assert f.file, "[$f.canonicalPath] - not found"
-                    zipfileset( file: f, prefix : 'server' )
-                }
+        assert pluginXmlFile.delete(), "Failed to delete temp file [${ pluginXmlFile.canonicalPath }]"
+    }
+
+
+    /**
+     * Creates temporary file with 'teamcity-plugin.xml' content.
+     *
+     * @param ext task extension
+     * @return temporary file with 'teamcity-plugin.xml' content, needs to be deleted later!
+     */
+    @Requires({ ext })
+    @Ensures({ result.file })
+    private File createPluginXml ( AssembleTeamCityPluginExtension ext )
+    {
+        final pluginXmlContent = this.class.getResourceAsStream( '/teamcity-plugin.xml' ).getText( 'UTF-8' ).
+                                 replace( '@name@',         project.name      ).
+                                 replace( '@version@',      version.toString()).
+                                 replace( '@display-name@', ext.displayName   ).
+                                 replace( '@description@',  ext.description   ).
+                                 replace( '@vendor-name@',  ext.vendorName    ).
+                                 replace( '@vendor-url@',   ext.vendorUrl     )
+
+        final pluginXmlFile    = File.createTempFile( project.name , null )
+
+        pluginXmlFile.deleteOnExit()
+        pluginXmlFile.write( pluginXmlContent )
+        pluginXmlFile
+    }
+
+
+    /**
+     * Archives server-side TeamCity plugin.
+     *
+     * @param ext       task extension
+     * @param pluginXml file with 'teamcity-plugin.xml' content
+     * @return          archive created
+     */
+    @Requires({ ext.serverConfigurations && pluginXml.file })
+    @Ensures ({ result.file })
+    private File archiveServerPlugin ( AssembleTeamCityPluginExtension ext, File pluginXml )
+    {
+        assert ( ext.serverJars || jarTask ), \
+               "No 'serverJars' specified and 'jar' task is not found in project [$project] - don't know what task archives your code"
+
+        File             destinationZip    = ext.destinationZip ?:
+                                             new File( project.buildDir, "teamcity/${ project.name }-${ version }.zip" )
+        Collection<File> pluginJars        = ( ext.serverJars ?: [ jarTask ] )*.archivePath
+        Collection<File> configurationJars = (( Collection<File> ) ext.serverConfigurations*.files.flatten().toSet()) -
+                                             project.configurations.getByName( 'teamcity' ).files
+
+        assert destinationZip.with { ( ! file ) || delete() }
+
+        ant.zip( destfile: destinationZip ) {
+
+            zipfileset( file: pluginXml, fullpath: 'teamcity-plugin.xml' )
+
+            ( pluginJars + configurationJars ).each {
+                File f ->
+                assert f.file , "[${ f.canonicalPath }] - not found"
+                zipfileset( file: f, prefix: 'server' )
             }
         }
-        else
-        {
-            throw new RuntimeException( 'Not supported yet' )
-        }
 
-        assert tempFile.delete()
+        destinationZip
     }
 }
