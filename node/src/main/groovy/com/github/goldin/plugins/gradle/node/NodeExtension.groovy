500e1100ee29fd12c1d3e9f4e4f5255c821bec06
Evgeny Goldin <evgenyg@gmail.com>
2012-11-12 17:12:58 +0100

    NodeExtension updates.

--- node/src/main/groovy/com/github/goldin/plugins/gradle/node/NodeExtension.groovy
+++ node/src/main/groovy/com/github/goldin/plugins/gradle/node/NodeExtension.groovy
@@ -4,7 +4,8 @@ package com.github.goldin.plugins.gradle.node
 @SuppressWarnings([ 'GroovyInstanceVariableNamingConvention', 'PropertyName' ])
 class NodeExtension
 {
-    boolean cleanWorkspace   = true // git checkout -f + git clean -dff
-    boolean cleanNodeModules = true // rm -rf ./node_modules
+    boolean cleanWorkspace   = true  // git checkout -f + git clean -dff
+    boolean cleanNodeModules = true  // rm -rf ./node_modules
+    boolean echoCommands     = true  // Echo all commands executed
     String  NODE_ENV         = 'development'
 }
