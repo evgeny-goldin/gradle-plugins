bd085b5e1aaf4a355f3b0afae2d9af69dc862739
Evgeny Goldin <evgenyg@gmail.com>
2012-11-19 16:09:43 +0100

    NodeExtension: boolean teamCityTests = false

--- node/src/main/groovy/com/github/goldin/plugins/gradle/node/NodeExtension.groovy
+++ node/src/main/groovy/com/github/goldin/plugins/gradle/node/NodeExtension.groovy
@@ -7,7 +7,9 @@ class NodeExtension
     boolean cleanWorkspace     = true  // git checkout -f + git clean -dff
     boolean cleanNodeModules   = true  // rm -rf ./node_modules
     boolean echoCommands       = true  // Echo all commands executed
-    boolean failOnTestFailures = true
+    boolean teamCityTests      = false // Whether test results should be displayed using TeamCity service messages (when job is run by TeamCity)
+    boolean failOnTestFailures = true  // Whether execution should fail when there are test failures.
+
     String  NODE_ENV           = 'development'
     String  nodeVersion        = 'latest'
     String  testCommand        = 'mocha'
