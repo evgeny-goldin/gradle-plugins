c21bca5e1468e9fcc8f760fc172ed54ec1bc131f
Evgeny Goldin <evgenyg@gmail.com>
2012-11-26 00:07:15 +0100

    Node plugin - "test", "stop" and "start" tasks don't depend on "setup".

--- node/src/main/groovy/com/github/goldin/plugins/gradle/node/NodePlugin.groovy
+++ node/src/main/groovy/com/github/goldin/plugins/gradle/node/NodePlugin.groovy
@@ -44,8 +44,6 @@ class NodePlugin extends BasePlugin
         final startTask    = project.tasks[ START_TASK     ] // by "super.apply( project )" call
 
         cleanAllTask.dependsOn cleanTask
-        testTask.    dependsOn setupTask
-        stopTask.    dependsOn setupTask
-        startTask.   dependsOn setupTask, stopTask
+        startTask.   dependsOn  stopTask
     }
 }
