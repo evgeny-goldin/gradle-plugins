68ff6c1b0a5b78c9c50e80ba2ff8fd5856185c08
Evgeny Goldin <evgenyg@gmail.com>
2013-01-28 00:15:06 +0100

    version=0.2  + "centralDeploy.sh" added

--- centralDeploy.sh
+++ centralDeploy.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 version=0.2
 # http://search.maven.org/#search%7Cgav%7C1%7Cg%3A%22org.apache.maven.plugins%22%20AND%20a%3A%22maven-gpg-plugin%22
-command="mvn org.apache.maven.plugins:maven-gpg-plugin:1.4:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging -Dgpg.passphrase=$gpgPassphrase"
+command="mvn -B -e org.apache.maven.plugins:maven-gpg-plugin:1.4:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging -Dgpg.passphrase=$gpgPassphrase"
 rootDir=`pwd`
 
 for module in 'about' 'common' 'crawler' 'duplicates' 'gitdump' 'kotlin' 'monitor' 'node' 'teamcity'
