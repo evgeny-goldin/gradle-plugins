e379372b3573aec56669c16237d5cbde67e8d883
Evgeny Goldin <evgenyg@gmail.com>
2013-02-25 17:43:45 +0100

    CodeNarc fixes.

--- node/src/main/groovy/com/github/goldin/plugins/gradle/node/tasks/HelpTask.groovy
+++ node/src/main/groovy/com/github/goldin/plugins/gradle/node/tasks/HelpTask.groovy
@@ -17,13 +17,17 @@ class HelpTask extends NodeBaseTask
         System.out.println( "- gradle $HELP_TASK         : displays this help message" )
         System.out.println( "- gradle $CLEAN_TASK        : deletes '${ scriptsFolder().name }' folder where all *.sh scripts are created" )
         System.out.println( "- gradle $CLEAN_MODULES : deletes '$NODE_MODULES_DIR' folder" )
-        System.out.println( "- gradle $SETUP_TASK        : installs Node.js and all application dependencies, runs automatically" )
+        System.out.println( "- gradle $SETUP_TASK        : installs Node.js and all application dependencies, " +
+                            "runs automatically" )
         System.out.println( "- gradle $TEST_TASK         : runs application unit tests" )
         System.out.println( "- gradle $START_TASK        : starts an application with 'forever'" )
         System.out.println( "- gradle $RESTART_ALL_TASK   : restarts all currently running applications" )
-        System.out.println( "- gradle $CHECK_STARTED_TASK : checks that application started is up and running, runs automatically after '$START_TASK' and '$RESTART_ALL_TASK'" )
-        System.out.println( "- gradle $STOP_TASK         : stops an application, runs automatically before '$START_TASK' and if '$CHECK_STARTED_TASK' fails" )
+        System.out.println( "- gradle $CHECK_STARTED_TASK : checks that application started is up and running, " +
+                            "runs automatically after '$START_TASK' and '$RESTART_ALL_TASK'" )
+        System.out.println( "- gradle $STOP_TASK         : stops an application, " +
+                            "runs automatically before '$START_TASK' and if '$CHECK_STARTED_TASK' fails" )
         System.out.println( "- gradle $STOP_ALL_TASK      : stops all currently running applications" )
-        System.out.println( "- gradle $CHECK_STOPPED_TASK : checks that application stopped is not accepting connections any more, runs automatically after '$STOP_TASK' and '$STOP_ALL_TASK'" )
+        System.out.println( "- gradle $CHECK_STOPPED_TASK : checks that application stopped is not accepting connections any more, " +
+                            "runs automatically after '$STOP_TASK' and '$STOP_ALL_TASK'" )
     }
 }
