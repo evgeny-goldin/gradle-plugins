6b98801073286610299610fe294cc4ba5ca2cdd1
Evgeny Goldin <evgenyg@gmail.com>
2013-03-03 15:16:58 +0100

    centralDeploy.sh update - reads version from "gradle.properties"

--- centralDeploy.sh
+++ centralDeploy.sh
@@ -3,13 +3,12 @@
 set -e
 set -o pipefail
 
-# http://search.maven.org/#search%7Cgav%7C1%7Cg%3A%22org.apache.maven.plugins%22%20AND%20a%3A%22maven-gpg-plugin%22
 # Requires:
 # 1) ~/.gnupg
 # 2) 'gpgPassphrase' environment variable
 # 3) 'sonatype-nexus-snapshots'/'sonatype-nexus-staging' credentials in "maven/conf/settings.xml"
 
-version=0.2.1
+version=`cat gradle.properties | grep 'version' | cut -d '=' -f 2`
 command="mvn -B -e org.apache.maven.plugins:maven-gpg-plugin:1.4:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging -Dgpg.passphrase=$gpgPassphrase"
 rootDir=`pwd`
 
