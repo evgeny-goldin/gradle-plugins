85cf654079ed675ebe7b6faaa89d800a11d6afcb
Evgeny Goldin <evgenyg@gmail.com>
2013-04-04 11:43:49 +0200

    gp-106: Add an option to create startup script - "BEGIN INIT INFO" removed, "su" added.

--- node/src/main/groovy/com/github/goldin/plugins/gradle/node/tasks/StartTask.groovy
+++ node/src/main/groovy/com/github/goldin/plugins/gradle/node/tasks/StartTask.groovy
@@ -85,16 +85,8 @@ class StartTask extends NodeBaseTask
         startupScript.write(
         """#!/bin/bash
         |
-        |### BEGIN INIT INFO
-        |# Provides: ${projectName}
-        |# Required-Start: \$remote_fs \$syslog
-        |# Required-Stop: \$remote_fs \$syslog
-        |# Default-Start: 2 3 4 5
-        |# Default-Stop: 0 1 6
-        |# Short-Description: ${projectName}
-        |# Description: ${projectName}
-        |### END INIT INFO
-        |
+        |su `whoami`
+        |cd
         |${ ext.stopallBeforeStart ? taskScriptFile( false, false, STOP_ALL_TASK ).canonicalPath : ext.stopBeforeStart ? taskScriptFile( false, false, STOP_TASK ).canonicalPath : '' }
         |${ ext.before             ? taskScriptFile( true ).canonicalPath : '' }
         |${ taskScriptFile().canonicalPath }
