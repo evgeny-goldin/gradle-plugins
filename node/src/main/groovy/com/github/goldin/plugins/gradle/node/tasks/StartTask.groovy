60536fd1d24ee2e04557b87f7bcb678e9095970f
Evgeny Goldin <evgenyg@gmail.com>
2013-04-04 11:06:11 +0200

    gp-106: Add an option to create startup script - "BEGIN INIT INFO" added (bug fix).

--- node/src/main/groovy/com/github/goldin/plugins/gradle/node/tasks/StartTask.groovy
+++ node/src/main/groovy/com/github/goldin/plugins/gradle/node/tasks/StartTask.groovy
@@ -87,8 +87,8 @@ class StartTask extends NodeBaseTask
         |
         |### BEGIN INIT INFO
         |# Provides: ${projectName}
-        |# Required-Start: $remote_fs $syslog
-        |# Required-Stop: $remote_fs $syslog
+        |# Required-Start: \$remote_fs \$syslog
+        |# Required-Stop: \$remote_fs \$syslog
         |# Default-Start: 2 3 4 5
         |# Default-Stop: 0 1 6
         |# Short-Description: ${projectName}
