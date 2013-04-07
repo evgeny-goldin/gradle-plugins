73ed323bbcadd5d942486dc9f127e62684207ef7
Evgeny Goldin <evgenyg@gmail.com>
2013-04-07 08:13:20 +0200

    version=0.2.4

--- centralDeploy.sh
+++ centralDeploy.sh
@@ -6,7 +6,7 @@ set -o pipefail
 # Requires:
 # 1) ~/.gnupg
 # 2) 'gpgPassphrase' environment variable
-# 3) 'sonatype-nexus-snapshots'/'sonatype-nexus-staging' credentials in "maven/conf/settings.xml"
+# 3) 'sonatype-nexus-snapshots'/'sonatype-nexus-staging' credentials in "$M2_HOME/conf/settings.xml"
 
 version=`cat gradle.properties | grep 'version' | cut -d '=' -f 2`
 command="mvn -B -e org.apache.maven.plugins:maven-gpg-plugin:1.4:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging -Dgpg.passphrase=$gpgPassphrase"
