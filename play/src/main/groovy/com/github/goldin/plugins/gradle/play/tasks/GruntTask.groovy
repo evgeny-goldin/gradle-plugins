92329433803322d97fcd1af4cbb21f903175aa08
Evgeny Goldin <evgenyg@gmail.com>
2013-10-21 00:22:00 +0200

    Play plugin, "grunt" task - 'Gruntfile.coffee' generated

--- play/src/main/groovy/com/github/goldin/plugins/gradle/play/tasks/GruntTask.groovy
+++ play/src/main/groovy/com/github/goldin/plugins/gradle/play/tasks/GruntTask.groovy
@@ -1,6 +1,8 @@
 package com.github.goldin.plugins.gradle.play.tasks
 
 import static com.github.goldin.plugins.gradle.play.PlayConstants.*
+import org.gcontracts.annotations.Ensures
+import org.gcontracts.annotations.Requires
 
 
 /**
@@ -16,7 +18,7 @@ class GruntTask extends PlayBaseTask
     }
 
 
-    void generatePackageJson()
+    private void generatePackageJson()
     {
         final variables = ( Map<String,?> ) ext.versions.collectEntries { String key, String value ->
             [ key.replace( '-', '_' ), value ]
@@ -27,25 +29,59 @@ class GruntTask extends PlayBaseTask
     }
 
 
-    void generateGruntFile()
+    private void generateGruntFile()
     {
         final cleanDestinations = []
-        final tasks             = [ 'clean' ]
+        final tasks             = []
+        final taskNames         = [ 'clean' ]
+        int counter             = 1
 
         for ( Map<String,?> map in ext.grunt )
         {
-            final source      = map['src']
-            final destination = map['dest']
+            final String source      = map[ 'src' ]
+            final String destination = map[ 'dest' ]
 
             assert source && destination, \
                    "Both 'src' and 'dest' should be defined for each 'grunt' element"
             cleanDestinations << destination
+
+            final isJs        = destination.endsWith( '.js' )
+            final isMinifyJs  = destination.endsWith( '.min.js' )
+            final isCss       = destination.endsWith( '.css' )
+            final isMinifyCss = destination.endsWith( '.min.css' )
+
+            assert isJs || isCss, "Illegal grunt destination '${destination}' - should end with '.js' or '.css'"
+
+            tasks << this."${ isJs ? 'jsTask' : 'cssTask' }"( source, destination, isJs ? isMinifyJs : isMinifyCss, counter++ )
         }
 
         final variables = [ packageJson       : PACKAGE_JSON,
                             cleanDestinations : cleanDestinations,
-                            tasks             : tasks ]
+                            tasks             : tasks,
+                            taskNames         : taskNames ]
 
         writeTemplate( GRUNT_FILE, GRUNT_FILE, variables )
+    }
+
+
+    @Requires({ source && destination && ( taskCounter > 0 ) })
+    @Ensures ({ result })
+    private String jsTask( Object source, String destination, boolean isMinify, int taskCounter )
+    {
+        """
+        |js:
+        |   ccc
+        """.stripMargin().trim()
+    }
+
+
+    @Requires({ source && destination && ( taskCounter > 0 ) })
+    @Ensures ({ result })
+    private String cssTask( Object source, String destination, boolean isMinify, int taskCounter )
+    {
+        """
+        |css:
+        |   ccc
+        """.stripMargin().trim()
     }
 }
--- play/src/main/resources/Gruntfile.coffee
+++ play/src/main/resources/Gruntfile.coffee
@@ -1,13 +1,15 @@
 module.exports = ( grunt ) ->
   grunt.initConfig
     pkg: grunt.file.readJSON( '$packageJson' )
+
 # https://github.com/gruntjs/grunt-contrib-clean
     clean:
       build: [ '${ cleanDestinations.join( "', '" ) }' ]
 
+  ${ tasks.join( '\n\n  ' ) }
 
   grunt.loadNpmTasks 'grunt-contrib-clean'
   grunt.loadNpmTasks 'grunt-contrib-coffee'
   grunt.loadNpmTasks 'grunt-contrib-uglify'
   grunt.loadNpmTasks 'grunt-contrib-less'
-  grunt.registerTask 'default', [ '${ tasks.join( "', '" ) }' ]
+  grunt.registerTask 'default', [ '${ taskNames.join( "', '" ) }' ]
